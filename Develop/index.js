97c79c836fa5fbee4bdeddcc02b6753edd851974
jack <biffisbuffm@gmail.com>
2020-07-25 19:53:42 -0500

    update

--- Develop/index.js
+++ Develop/index.js
@@ -1,143 +1,151 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+//const markdown = require("./utils/generateMarkdown");
 
 
-
-
-// array of questions
-const promptUser = () => {
-return inquirer.prompt([
-    {
-        type: 'input',
-        name: 'name',
-        message: 'What is your project name? (Required)',
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log('Please enter your name');
-                return false;
-            }
-        }
-    },
-    {
-        type:'input',
-        name: 'description',
-        message: 'Enter project description (Required)',
-        validate: nameInput => {
-            if (nameInput) {
-                if (nameInput) {
-                    return true;
-                } else {
-                    console.log('Please enter a description');
-                    return false;
-                }
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'tableOfContents',
-        message: 'Enter table of contents',
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log('Please enter information');
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name:'installation',
-        message: 'Enter installation instructions (Required)',
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log('Please enter information!');
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'Enter usage for your project (Required)',
-        validate: nameInput =>{
-            if (nameInput) {
-                return true;
-            } else {
-                console.log('Please enter information!');
-                return false;
-            }
-        }
-    },
-    // this needs to be a dropdown bar{
-        type: 'list',
-        name: 'license',
-        message: 'Enter licensing information (Required)',
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log('Please enter information!');
-                return false;
-            }
-        }
-    //},
-    {
-        type: 'input',
-        name: 'contributing',
-        message: 'Enter contributor information (Required)',
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log('Please enter information');
-                return false;
-            }
-        }
-
-    },
-    {
-        type: 'input',
-        name: 'tests',
-        message: 'Enter testing information (Required)',
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log('Please enter information');
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'questions',
-        message: 'Enter questions (Required)',
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log('Please enter information');
-                return false;
-            }
-        }
-    }
-
-
-
-    ])
-}
 
 
 // array of questions for user
 const questions = [
-
+    promptUser = () => {
+        return inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is your project name? (Required)',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter your name');
+                        return false;
+                    }
+                }
+            },
+            {
+                type:'input',
+                name: 'description',
+                message: 'Enter project description (Required)',
+                validate: nameInput => {
+                    if (nameInput) {
+                        if (nameInput) {
+                            return true;
+                        } else {
+                            console.log('Please enter a description');
+                            return false;
+                        }
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'tableOfContents',
+                message: 'Enter table of contents',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter information');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name:'installation',
+                message: 'Enter installation instructions (Required)',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter information!');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'usage',
+                message: 'Enter usage for your project (Required)',
+                validate: nameInput =>{
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter information!');
+                        return false;
+                    }
+                }
+            },
+            // // this needs to be a dropdown bar{
+            //     type: 'list',
+            //     name: 'license',
+            //     message: 'Enter licensing information (Required)',
+            //     validate: nameInput => {
+            //         if (nameInput) {
+            //             return true;
+            //         } else {
+            //             console.log('Please enter information!');
+            //             return false;
+            //         }
+            //     }
+            // //},
+            {
+                type: 'input',
+                name: 'contribution',
+                message: 'Enter contributor information (Required)',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter information');
+                        return false;
+                    }
+                }
+        
+            },
+            {
+                type: 'input',
+                name: 'tests',
+                message: 'Enter testing information (Required)',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter information');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'questions',
+                message: 'Enter questions (Required)',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter information');
+                        return false;
+                    }
+                }
+            }
+        
+        
+        
+            ]);
+        }
 ];
+
+inquirer.prompt(questions)
+    .then((inquirerResponse, data) => {
+        console.log("Making ReadMe");
+        fs.writeFileSync("ReadMe.md", inquirerRespoonse, data);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+
+
 
 // function to write README file
 function writeToFile(fileName, data) {
---
+++ Develop/utils/generateMarkdown.js
@@ -0,0 +1,36 @@
+// function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+  ${data.username}
+  ##username:
+
+  ${data.description}
+  ##description:
+
+  ${data.tableOfContents}
+  ##table of contents:
+
+  ${data.installation}
+  ##installation:
+
+  ${data.usage}
+  ##usage:
+
+  ${data.licenses}
+  ##licenses:
+
+  ${data.contribution}
+  ##contribution:
+
+  ${data.test}
+  ##test:
+
+  ${data.questions}
+  ##questions:
+
+  
+`;
+}
+
+module.exports = generateMarkdown;
